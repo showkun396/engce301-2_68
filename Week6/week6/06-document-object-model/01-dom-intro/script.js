45ca527588ac47a53f67294e81d02c56163da8ae
poomrapee <poomrapee.8652@gmail.com>
2025-12-29 20:32:57 +0700

    test

--- Week6/week6/06-document-object-model/01-dom-intro/script.js
+++ Week6/week6/06-document-object-model/01-dom-intro/script.js
@@ -13,8 +13,9 @@ console.log(document.links[0]);
 
 // The document object has a ton of methods. One is `document.write()`, which will write something to the document
 document.write('Hello from JS');
+document.write('how are you');
 
 // We also have methods to select elements more directly
 document.getElementById('main').innerHTML = '<h1>Hello from main</h1>';
 
-document.querySelector('#main h1').innerText = 'Hello';
+document.querySelector('#main h1').innerText = 'Hello from main';
